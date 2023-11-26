d64fbc6f05fa45eb03f526a1de4ae720d1a83984
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-11-26 20:25:48 +0600

    Added AuthContext

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
                         {/* <!-- Left column container with background--> */}
                         <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
                             <img
-                                src="https://i.ibb.co/bJYjX8J/login-removebg-preview.png"
+                                src="https://i.ibb.co/T1pt0NY/loginb-removebg-preview.png"
                                 className="w-full"
                                 alt="Phone image" />
                         </div>
@@ -97,7 +97,7 @@ const Register = () => {
                                     className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                                     data-te-ripple-init
                                     data-te-ripple-color="light">
-                                    Sign in
+                                    Register
                                 </button>
 
                                 {/* <!-- Divider --> */}
